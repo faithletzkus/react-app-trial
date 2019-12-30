5a9f8597ffd70ba3394a341045042950f0d4010e
Faith Letzkus <faith.letzkus@gmail.com>
2019-12-29 20:29:07 -0600

    Updates

---
+++ precache-manifest.c92bd1ecf8ecfdc14a5c610051200616.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16be5c8ebc9bf8ca8a265e38d280ec32",
+    "url": "/react-app-trial/index.html"
+  },
+  {
+    "revision": "d76f7f13c6f65664cf56",
+    "url": "/react-app-trial/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "257c11b9873b7205c33d",
+    "url": "/react-app-trial/static/js/2.7e8da871.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-app-trial/static/js/2.7e8da871.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d76f7f13c6f65664cf56",
+    "url": "/react-app-trial/static/js/main.bf0ba7b8.chunk.js"
+  },
+  {
+    "revision": "4bbd2ea9e0d2e9578660",
+    "url": "/react-app-trial/static/js/runtime-main.2b3fc468.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-trial/static/media/logo.5d5d9eef.svg"
+  }
+]);
